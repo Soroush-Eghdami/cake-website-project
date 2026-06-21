fa440e8dab20d49ccb93e4e509c00df94d2b4414
soroush eghdami <soroush.egh@gmail.com>
2026-06-21 22:01:35 +0330

    fixing product admin.py

--- Back/products/admin.py
+++ Back/products/admin.py
@@ -3,6 +3,13 @@ from .models import Category, Product
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    search_fields = ('name',)
+admin.site.register(Category, CategoryAdmin)
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'category', 'price')
+    search_fields = ('name',)
+    list_filter = ('category',)
+admin.site.register(Product, ProductAdmin)
